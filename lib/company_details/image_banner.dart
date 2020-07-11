021c5fa66a5220cf66b361df1b2b9274b9714914
Helen Brüggmann <helenbruggmann@Helens-MacBook-Pro.local>
2020-07-11 23:11:07 +0200

    company details screen

--- lib/company_details/image_banner.dart
+++ lib/company_details/image_banner.dart
@@ -8,7 +8,7 @@ class ImageBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-      
+      color: _color,
       ),
       child: Text('Hi'),
     );
