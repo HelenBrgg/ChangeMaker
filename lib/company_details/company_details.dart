cf6974749f474825f3a60465fe728280f86f2405
Freia Then <freia.then@1und1.de>
2020-07-12 09:41:28 +0200

    rewrite company detail

--- lib/company_details/company_details.dart
+++ lib/company_details/company_details.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
@@ -6,7 +8,14 @@ import 'text_section.dart';
 
 
 class CompanyDetail extends StatelessWidget {
-  
+  final int _company;
+  final String _imagePath;
+  final String _name;
+  final String _description;
+  final IconData _iconSymbol;
+  final Color _iconColor;
+
+  CompanyDetail(this._company, this._imagePath, this._name, this._description,  this._iconSymbol, this._iconColor);
 
 
   Widget build(BuildContext context) {
@@ -18,9 +27,55 @@ class CompanyDetail extends StatelessWidget {
           mainAxisAlignment:MainAxisAlignment.start,
           crossAxisAlignment:CrossAxisAlignment.stretch,
           children: [
-            ImageBanner("assets/images/1_bad-company-logo_scaled.jpg"),
-            TextSection("RVVE","You caught this company doing greenwashing... what a shame"),
+            ImageBanner(getCompanyImage(_company, _imagePath)),
+            TextSection(getCompanyName(_company, _name), getCompanyDetail(_company, _description), getIconSymbol(_company, _iconSymbol), getIconColor(_company, _iconColor)),
           ],
         ));
   }
+
+  getCompanyImage(company, imagePath){
+    if (company==0){
+      imagePath = "assets/images/1_bad-company-logo_scaled.jpg";
+    } else {
+      imagePath = "assets/images/2_good-company-logo_scaled.jpg";
+    }
+  }
+
+  getCompanyName(company, name){
+    if(company==0){
+      name = "RVVE";
+    } else {
+      name = "Natüre";
+    }
+    return name;
+  }
+
+  getCompanyDetail(company, description){
+    if(company==0){
+      description = "You caught this company doing greenwashing... what a shame";
+    } else {
+      description = "What a great sustainable company";
+    }
+    return description;
+  }
+
+  getIconSymbol(company, symbol){
+    if(company==0){
+      symbol = Icons.remove_circle;
+    } else {
+      symbol = Icons.add;
+    }
+    return symbol;
+  }
+
+  getIconColor(company, color){
+    if(company==0){
+      color = Colors.black;
+    } else {
+      color = Colors.lightGreen;
+    }
+    return color;
+  }
+
 }
+
--- lib/company_details/text_section.dart
+++ lib/company_details/text_section.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 class TextSection extends StatelessWidget {
   final String _title;
   final String _body;
+  final IconData _iconSymbol;
+  final Color _iconColor;
   static const double _hPad=16.0;
 
-  TextSection(this._title, this._body);
+  TextSection(this._title, this._body, this._iconSymbol, this._iconColor);
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -17,12 +19,21 @@ class TextSection extends StatelessWidget {
         child: Text(_title, style: Theme.of(context).textTheme.title),),
         Container(
           padding: const EdgeInsets.fromLTRB(_hPad,10.0,_hPad,_hPad),
-        child: new Icon(Icons.remove_circle, color: Colors.black, size: 50),),
+            child: new Icon(_iconSymbol, color: _iconColor, size:50),),
         Container(
           padding: const EdgeInsets.fromLTRB(_hPad,10.0,_hPad,_hPad),
         child: Text(_body,style: Theme.of(context).textTheme.body1),)
         ],
     );
     
+  }
+  getIcon(_company,_icon){
+    if (_company== 0){
+      _icon = new Icon(Icons.remove_circle, color: Colors.black, size: 50);
+    }else {
+      _icon = new Icon(Icons.add, color: Colors.lightGreen, size: 50);
+    }
+
+    return _icon;
   }
 }
