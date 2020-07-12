746001f32d5af060ef9180f59b4785f7453fc129
Helen Brüggmann <helenbruggmann@Helens-MacBook-Pro.local>
2020-07-12 08:36:39 +0200

    icons hard coding

--- lib/style.dart
+++ lib/style.dart
@@ -8,7 +8,7 @@ const BodyTextSize = 16.0;
 const String FontNameDefault = 'Montserrat';
 
 const AppBarTextStyle = TextStyle(fontFamily : FontNameDefault,
-fontWeight: FontWeight.w300, fontSize:MediumTextSize, color:Colors.white)
+fontWeight: FontWeight.w300, fontSize:MediumTextSize, color:Colors.white);
 
 const TitleTextStyle= TextStyle(fontFamily:FontNameDefault,
 fontWeight:FontWeight.w300,
